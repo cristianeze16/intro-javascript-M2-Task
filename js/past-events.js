564fbb39fe87a5a65d88b21c34c6f2a88231ca0f
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-10-06 18:54:52 -0300

    imprimiendo desde js cards past,future y events

--- js/past-events.js
+++ js/past-events.js
@@ -1,29 +1,71 @@
-let container = document.getElementById("card");
+const container = document.getElementById("card-past");
+const currentDate = data.currentDate;
+const dataEvents = data.events;
 
+const pastEvents = dateFilter(dataEvents, currentDate);
 
-for (let i = 0; i < data.events.length; i++) {
-  let div = document.createElement("div");
+for (let past of pastEvents) {
+  makeCards(past, container);
+}
 
-  div.innerHTML = `
-
-
- <div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
-      <img src="${data.events[i].image}" class="card-img-top" alt="${data.events[i].name}">
+function makeCards(data, contenedor) {
+  contenedor.innerHTML += `
+<div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
+      <img src="${data.image}" class="card-img-top" alt="${data.name}">
       <div class="card-body ">
-        <h5 class="card-title text-center">${data.events[i].name}</h5>
-        <p class="card-text ">${data.events[i].description}
+        <h5 class="card-title text-center">${data.name}</h5>
+        <p class="card-text ">${data.description}
         </p>
       </div>
 
       <div class="card-body">
-        <p>Price :$${data.events[i].price}</p>
+        <p>Price :$${data.price}</p>
         <a href="details_cocina.html" class="card-link text-white">Details</a>
       </div>
     </div>
-
-
-
-
   `;
-  container.appendChild(div);
 }
+
+function dateFilter(data, current) {
+  let pastEvents = [];
+  let dateCmp;
+  let date;
+  let currentDateCmp;
+  for (let i = 0; i < data.length; i++) {
+    date = data[i].date;
+    dateCmp = new Date(date);
+    currentDateCmp = new Date(current);
+    if (dateCmp < currentDateCmp) {
+      pastEvents.push(data[i]);
+    }
+  }
+  return pastEvents;
+}
+
+
+
+
+// let container = document.getElementById("card");
+
+// for (let i = 0; i < data.events.length; i++) {
+//   let div = document.createElement("div");
+
+//   div.innerHTML = `
+
+//  <div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
+//       <img src="${data.events[i].image}" class="card-img-top" alt="${data.events[i].name}">
+//       <div class="card-body ">
+//         <h5 class="card-title text-center">${data.events[i].name}</h5>
+//         <p class="card-text ">${data.events[i].description}
+//         </p>
+//       </div>
+
+//       <div class="card-body">
+//         <p>Price :$${data.events[i].price}</p>
+//         <a href="details_cocina.html" class="card-link text-white">Details</a>
+//       </div>
+//     </div>
+
+//   `;
+//   container.appendChild(div);
+// }
--- js/upcoming-events.js
+++ js/upcoming-events.js
@@ -1,46 +1,43 @@
-let currentDate = data.currentDate;
-let date = data.events[0].date;
-// const future = dateFilter(data, currentDate);
-const futureEvents= dateFilter(data, currentDate);
+const container = document.getElementById("card-upcomming");
+const currentDate = data.currentDate;
+const dataEvents = data.events;
 
+const futureEvents = dateFilter(dataEvents, currentDate);
 
-for(let future of futureEvents){
-  makeCards(future,container);
+for (let future of futureEvents) {
+  makeCards(future, container);
 }
 
+function makeCards(data, contenedor) {
+  contenedor.innerHTML += `
+<div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
+      <img src="${data.image}" class="card-img-top" alt="${data.name}">
+      <div class="card-body ">
+        <h5 class="card-title text-center">${data.name}</h5>
+        <p class="card-text ">${data.description}
+        </p>
+      </div>
 
-
-
-
-
-
-
-
-
-
-
-
+      <div class="card-body">
+        <p>Price :$${data.price}</p>
+        <a href="details_cocina.html" class="card-link text-white">Details</a>
+      </div>
+    </div>
+  `;
+}
 
 function dateFilter(data, current) {
-  // futureEvent = false;
-  let upcomming=[];
-  for (let i = 0; i < data.events.length; i++) {
-    let date = data.events[i].date;
-    let dateCmp = new Date(date);
-    let currentDateCmp = new Date(current);
+  let upcomming = [];
+   let dateCmp;
+   let date;
+   let currentDateCmp;
+  for (let i = 0; i < data.length; i++) {
+     date = data[i].date;
+     dateCmp = new Date(date);
+    currentDateCmp = new Date(current);
     if (dateCmp > currentDateCmp) {
-    // futureEvent = true;
-      // console.log("FUTURE EVENT");
-      // console.log(currentDateCmp + "ACtualidad");
-      // console.log(dateCmp);
-    upcomming.push(data[i]);
-
-
-    
-    }
-    
-    // return futureEvent;
-    console.log(upcomming)
+     upcomming.push(data[i])
+    }    
   }
+  return upcomming;
 }
-
