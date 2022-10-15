31f7ba51fe814a8e3be5649acaf71c6a0c01b30f
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-10-15 16:38:45 -0300

    chebox ok

--- js/index.js
+++ js/index.js
@@ -1,13 +1,40 @@
 const container = document.getElementById("card");
 const events = data.events;
+let checkbox = document.getElementById("check");
+let checkItem = [];
+printCardCategory(events);
 
-for (let card of events) {
-  makeCards(card, container);
+
+checkbox.addEventListener("click", function (event) {
+  let checked = event.target.checked;
+  let value = event.target.value;
+  if (checked) {
+    checkItem.push(value);
+  } else {
+    checkItem = checkItem.filter((item) => item != value);
+  }
+  filter(checkItem);
+});
+
+function filter(item) {
+  let event = events;
+  
+ 
+    event= event.filter((e) => item.includes(e.category));
+    container.innerHTML=''; // limpiar antes de volver a imprimir
+    printCardCategory(event);
+    console.log(event);
+}
+
+function printCardCategory(events) {
+  for (let card of events) {
+    makeCards(card, container);
+  }
 }
 
 function makeCards(data, contenedor) {
   contenedor.innerHTML += `
- <div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
+ <div  class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
       <img src="${data.image}" class="card-img-top" alt="${data.name}">
       <div class="card-body ">
         <h5 class="card-title text-center">${data.name}</h5>
@@ -22,29 +49,3 @@ function makeCards(data, contenedor) {
     </div>
   `;
 }
-
-
-// let container = document.getElementById("card");
-// for (let i = 0; i < data.events.length; i++) {
-//   let div = document.createElement("div");
-
-//   div.innerHTML = `
-
-//  <div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
-//       <img src="${data.events[i].image}" class="card-img-top" alt="${data.events[i].name}">
-//       <div class="card-body ">
-
-//         <h5 class="card-title text-center">${data.events[i].name}</h5>
-//         <p class="card-text ">${data.events[i].description}
-//         </p>
-//       </div>
-
-//       <div class="card-body">
-//         <p>Price :$${data.events[i].price}</p>
-//         <a href="details_cocina.html" class="card-link text-white">Details</a>
-//       </div>
-//     </div>
-
-//   `;
-//   container.appendChild(div);
-// }
