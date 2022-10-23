92c5526e09d9174cb95932cdb59d137aa34243f5
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-10-23 00:27:48 -0300

    past-events

--- js/past-events.js
+++ js/past-events.js
@@ -1,119 +1,65 @@
-const container = document.getElementById("card-past");
-const currentDate = data.currentDate;
-const dataEvents = data.events;
-const inputSearch = document.getElementById("js-search");
-const button = document.getElementById("button-submit");
-const checkbox = document.getElementById("check");
-const pastEvents = dateFilter(dataEvents, currentDate);
+async function getPast() {
+  try {
+    let response = await fetch(
+      "https://mind-hub.up.railway.app/amazing?time=past"
+    );
+    let data = await response.json();
+    let events = data.events;
+    const checkbox = document.getElementById("check");
+    const inputSearch = document.getElementById("js-search");
+    const container = document.getElementById("card-past");
 
+    function createCheckbox(event, container) {
+      let checkBoxData = new Set(event.map((element) => element.category));
+      checkBoxData = [...checkBoxData];
+      for (let check of checkBoxData) {
+        makeCategory(check, container);
+      }
+    }
 
-let checkItem = [];
-let applied = {};
+    updateCard(events, container);
+    createCheckbox(events, checkbox);
 
-createCheckbox(pastEvents, checkbox);
+    checkbox.addEventListener("change", filterSearch);
+    inputSearch.addEventListener("keyup", filterSearch);
 
-function createCheckbox(event, container) {
-  let checkBoxData = new Set(event.map((element) => element.category));
-  checkBoxData = [...checkBoxData];
-  for (let check of checkBoxData) {
-    makeCategory(check, container);
-  }
-}
+    function filterSearch() {
+      let checked = [
+        ...document.querySelectorAll('input[type="checkbox"]:checked'),
+      ].map((element) => element.value);
+      let filterchecked = events.filter(
+        (check) => checked.includes(check.category) || checked.length === 0
+      );
+      let filterText = filterchecked.filter((text) =>
+        text.name.toLowerCase().includes(inputSearch.value.toLowerCase())
+      );
+      updateCard(filterText, container);
+    }
 
-function makeCategory(data, containerCategory) {
-  // console.log(containerCategory)
-  containerCategory.innerHTML += `
+     function updateCard(events, element) {
+       element.innerHTML = "";
+       if (events.length > 0) {
+         for (let card of events) {
+           makeCards(card, container);
+         }
+       } else {
+         element.innerHTML = `<h1 class="text-white text-bg-secondary bg-opacity-50 "> Oops not found</h1>`;
+       }
+     }
+    function makeCategory(data, containerCategory) {
+      // console.log(containerCategory)
+      containerCategory.innerHTML += `
          <div class="form-check form-check-inline ">
           <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="${data}">
           <label class="form-check-label" for="inlineCheckbox1">${data}</label>
         </div>
   
   `;
-}
-
-updateCard(pastEvents, container);
-
-function filterBoth(fn, value) {
-  let event = pastEvents;
-  applied[fn] = value;
-
-  for (let name in applied) {
-    if (name === "isCheck") {
-      event = event.filter((echeck) => applied[name].includes(echeck.category));
     }
-    if (name === "matchesWithText") {
-      event = event.filter((etext) =>
-        etext.name.toLowerCase().includes(applied[name].toLowerCase())
-      );
-    }
-  }
-   if (event.length === 0) {
-     console.log(event);
-     applied = {};
-   }
-  return event;
-}
 
-checkbox.addEventListener("click", function (event) {
-  let checked = event.target.checked;
-  let value = event.target.value;
-  if (checked) {
-    checkItem.push(value);
-  } else {
-    checkItem = checkItem.filter((item) => item != value);
-  }
-  filter(checkItem);
-});
-
-inputSearch.addEventListener("input", function (ev) {
-  let event;
-  event = filterBoth("matchesWithText", ev.target.value);
-  if (ev.target.value === "") {
-   
-    updateCard(pastEvents, container);
-  }
-    updateCard(event, container);
-  
-});
-
-
-
-function filter(item) {
-  let event;
-  event = filterBoth("isCheck", item);
-  updateCard(event, container);
-  if (item.length === 0) {
-   
-    updateCard(pastEvents, container);
-  }
-}
-
-function updateCard(events, element) {
-  element.innerHTML = ""; // limpiar antes de volver a imprimir
-  for (let card of events) {
-    makeCards(card, container);
-  }
-}
-
-function dateFilter(data, current) {
-  let pastEvents = [];
-  let dateCmp;
-  let date;
-  let currentDateCmp;
-  for (let i = 0; i < data.length; i++) {
-    date = data[i].date;
-    dateCmp = new Date(date);
-    currentDateCmp = new Date(current);
-    if (dateCmp < currentDateCmp) {
-      pastEvents.push(data[i]);
-    }
-  }
-  return pastEvents;
-}
-
-function makeCards(data, contenedor) {
-  contenedor.innerHTML += `
-<div class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
+    function makeCards(data, contenedor) {
+      contenedor.innerHTML += `
+ <div  class="card-size  card text-bg-secondary bg-opacity-50" style="width: 18rem;">
       <img src="${data.image}" class="card-img-top" alt="${data.name}">
       <div class="card-body ">
         <h5 class="card-title text-center">${data.name}</h5>
@@ -127,5 +73,10 @@ function makeCards(data, contenedor) {
       </div>
     </div>
   `;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
+getPast();
