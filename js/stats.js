3408891298ee8ffaafa894c743aa599b6380069a
cristianeze16 <cristian.ezequiel.hermosa@gmail.com>
2022-10-24 14:18:19 -0300

    tabla

--- js/stats.js
+++ js/stats.js
@@ -5,35 +5,31 @@ async function getPastStatsTable1() {
     events = data.events;
     let table1 = document.getElementById("table-1");
     let table3 = document.getElementById("table-3");
+
     events.map((pastEvents) => {
-      pastEvents.assistancePerCenth = (
-        (100 * pastEvents.assistance /pastEvents.capacity)).toFixed(2);
+      pastEvents.assistancePerCenth = (100 * pastEvents.assistance /pastEvents.capacity).toFixed(2);
       pastEvents.gain = pastEvents.price * pastEvents.assistance;
     });
-    console.log(events)
-    let ordenPorAsist = [...events].sort(
-      (event1, event2) => event1.assistancePerCenth - event2.assistancePerCenth
-    );
+  
+    let ordenPorAsist = [...events].sort((event1, event2) => event1.assistancePerCenth - event2.assistancePerCenth);
     let menorAsistencia = ordenPorAsist[0];
     let mayorAsistencia = ordenPorAsist[ordenPorAsist.length - 1];
-
-    let ordenPorCapacidad = [...events].sort(
-      (event1, event2) => event1.capacity - event2.capacity
-    );
+    let ordenPorCapacidad = [...events].sort((event1, event2) => event1.capacity - event2.capacity);
     let mayorCapacidad = ordenPorCapacidad[ordenPorCapacidad.length - 1];
+
+    printTable1(table1, menorAsistencia, mayorAsistencia, mayorCapacidad);
 
     let categories = new Set(events.map((element) => element.category));
     categories = [...categories];
+
     let filtercategories = categories.map((categoria) => {
       let filterReduce = events.filter((event) => event.category == categoria);
       return reduce(filterReduce);
     });
-
+  
     for (let array of filtercategories) {
       printTable3(array, table3);
-    } 
-
-
+    }
 
     function reduce(filter) {
       let initial = {
@@ -42,53 +38,34 @@ async function getPastStatsTable1() {
         assistance: 0,
         capacity: 0,
       };
-      let result = filter.reduce((element1, element2) => {
+      let result = filter.reduce((variableAccumulador, element2) => {
         return {
           category: element2.category,
-          gain: element1.gain + element2.gain,
-          assistance: element1.assistance+element2.assistance,
-          capacity: element1.capacity + element2.capacity,
+          gain: variableAccumulador.gain + element2.gain,
+          assistance: variableAccumulador.assistance + element2.assistance,
+          capacity: variableAccumulador.capacity + element2.capacity,
         };
       }, initial);
-      result.percenth = (100 *  result.assistance/result.capacity).toFixed(2);
-      console.log(result);
+      result.percenth = (100 * result.assistance / result.capacity).toFixed(2);
       return result;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    printTable1(table1, menorAsistencia, mayorAsistencia, mayorCapacidad);
-  } catch (error) {
-    
-  }
-
-
   
+  } catch (error) {}
 }
+
 function printTable3(dataTable, container) {
-  console.log(dataTable);
   container.innerHTML += `
-                
-                <td>${dataTable.category}</td>
-                <td>${dataTable.gain}</td>
-                <td>${dataTable.percenth} %</td>           
-              
+  <td>${dataTable.category}</td>
+  <td>$${dataTable.gain}</td>
+  <td>${dataTable.percenth} %</td> 
    `;
 }
 
 getPastStatsTable1();
 
-function printTable1(container,minAsistencia,maxAsistencia,maxCapacidad) { 
-  container.innerHTML = `<tr>     
+function printTable1(container, minAsistencia, maxAsistencia, maxCapacidad) {
+  container.innerHTML = `<tr>
             <td>Events with the min percentage of attendance</td>
             <td>Events with the hightest percentage of attendance</td>
             <td>Events with larger</td>
@@ -104,121 +81,55 @@ function printTable1(container,minAsistencia,maxAsistencia,maxCapacidad) {
             <td>${maxCapacidad.capacity}</td>
           </tr>
   `;
-  
 }
 
-
-async function getUpStatsTable2(){
-
-  try {
-      response = await fetch("https://mind-hub.up.railway.app/amazing?time=upcoming")
-      data =await response.json();
-      events = data.events;  
-      events.map(up=>{
-        up.gain = up.price * up.estimate;
-        up.assistance = 100* up.estimate/up.capacity})
-      
-      let table2= document.getElementById("table-2")
-      let categories = new Set(events.map((element) => element.category));
-      categories = [...categories];
-      let filtercategories = categories.map(categoria=>{
-        let filterReduce= events.filter(event => event.category==categoria)
-        return reduce(filterReduce)
-      })
-        for(let array of filtercategories){printTable2(array,table2)}// esta se usa para imprimir 
-      
-
-      
-
-
-
-
-
-function reduce(filter){
-let initial={
-  category:"",
-  gain:0,
-  assistance: 0,
-}
-let result= filter.reduce((element1,element2)=>{
-  return{
-    category:element2.category,
-    gain:element1.gain + element2.gain,
-    assistance:element2.assistance,
-  }
-},initial)
-
-return result
-
-}
-
-  } catch (error) {
-    
-  }
-
-}
-getUpStatsTable2();
-
-
-function printTable2(dataTable,container) {
-  console.log(dataTable)
-   container.innerHTML += `
-                
-                <td>${dataTable.category}</td>
-                <td>${dataTable.gain}</td>
-                <td>${dataTable.assistance} %</td>           
-              
-   `;
-  
-
-
-
-
-
-}
-
-
-
-async function getPastStatsTable3() {
+async function getUpStatsTable2() {
   try {
     response = await fetch(
-      "https://mind-hub.up.railway.app/amazing?time=past"
+      "https://mind-hub.up.railway.app/amazing?time=upcoming"
     );
     data = await response.json();
     events = data.events;
-
-    events.map((past) => {
-      past.gain = past.price * past.assistance;
+    events.map((up) => {
+      up.gain = up.price * up.estimate;
+      up.estimates = (100 * up.estimate) / up.capacity;
     });
-    console.log(events);
 
+    let table2 = document.getElementById("table-2");
     let categories = new Set(events.map((element) => element.category));
     categories = [...categories];
-    let filtercategories = categories.map((categoria) => {
-      let filterReduce = events.filter((event) => event.category == categoria);
+
+    let filtercategories = categories.map((categoria) => {let filterReduce = events.filter((event) => event.category == categoria);
       return reduce(filterReduce);
     });
-    console.log(filtercategories); // esta se usa para imprimir
+    for (let array of filtercategories) {
+      printTable2(array, table2);
+    }
 
     function reduce(filter) {
-    
       let initial = {
         category: "",
         gain: 0,
-        capacity: 0,
+        estimates: 0,
       };
-      let result = filter.reduce((element1, element2) => {
+      let result = filter.reduce((variableAccumulador, element2) => {
         return {
           category: element2.category,
-          gain: element1.gain + element2.gain,
-          capacity: element1.capacity + element2.capacity,
+          gain: variableAccumulador.gain + element2.gain,
+          estimates: element2.estimates,
         };
       }, initial);
 
       return result;
     }
-   
   } catch (error) {}
 }
-getPastStatsTable3();
+getUpStatsTable2();
 
+function printTable2(dataTable, container) {
+  container.innerHTML += `
+  <td>${dataTable.category}</td>
+  <td>$${dataTable.gain}</td>
+  <td>${dataTable.estimates} %</td>
+   `;
+}
